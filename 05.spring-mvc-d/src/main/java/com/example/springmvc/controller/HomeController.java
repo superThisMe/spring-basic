6d2461c555d9fe0adbe72c458ad90f6522937bfa
superthisme <apprentice91@gmail.com>
2019-12-19 17:16:54 +0900

    spring-basic 19/12/19
    Spring mvc practice

---
+++ 05.spring-mvc-d/src/main/java/com/example/springmvc/controller/HomeController.java
@@ -0,0 +1,30 @@
+package com.example.springmvc.controller;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller // @Component + spring mvc 기능 추가
+@RequestMapping(path = { "/" })
+public class HomeController {
+	
+	//@RequestMapping : 요청과 메서드를 매핑
+	@GetMapping
+	public String home(Locale locale, Model model) {
+
+		Date date = new Date();
+		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+		
+		String formattedDate = dateFormat.format(date);
+		
+		model.addAttribute("serverTime", formattedDate );
+		
+		return "home"; // viewname -> /WEB-INF/views/ + home + .jsp
+	}
+	
+}
